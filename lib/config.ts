cb872fbd213acb58be42ec8ab05da11197c5c510
Yuichi Yazaki <visualizingjp@gmail.com>
2026-01-11 23:46:53 +0900

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,9 +2,6 @@ export const APP_CONFIG = {
     DOMAIN: ".dataviz.jp",
     COOKIE_NAME: "sb-dataviz-auth-token",
     ALLOWED_ORIGINS: [
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002",
         "https://rawgraphs.dataviz.jp",
         "https://kepler-gl.dataviz.jp",
         "https://cartogram-japan.dataviz.jp",
