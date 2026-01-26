aa1b11fcc82ca04385f28baf7045fc703e393463
Yuichi Yazaki <visualizingjp@gmail.com>
2026-01-26 11:20:38 +0900

    ヒーローコンテンツの差し替え

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
         <p className="text-3xl lg:text-4xl !leading-tight">
           データ可視化とデータ加工の便利ツール集
         </p>
-        <br />
+        <br /><br /><br />
         <p className="mt-6 text-base leading-relaxed text-muted-foreground">
           本サービスは現在、過去にご利用いただいたお客さま向けのクローズドテスト期間です。招待コードをお持ちの方のみご利用いただけます。フィードバックをもとに機能改善を行った後、一般公開を予定しています。
         </p>
